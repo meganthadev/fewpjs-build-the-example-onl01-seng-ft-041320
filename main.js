70a44c1f56df8de461f444d1d843880fc34db5fb
Megan Horsfield <meganthadev@gmail.com>
2020-08-02 07:18:54 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,38 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+let errorMessage = document.getElementById('modal')
+let hearts = document.querySelectorAll('.like-glyph')
 
+document.addEventListener("DOMContentLoaded", () => {
+  errorMessage.setAttribute('class', 'hidden')
+});
+
+for (let heart of hearts){
+  heart.addEventListener('click', (e) => {
+    changeHeart(e)
+  })
+}
+
+function changeHeart(e){
+  let heart = e.target
+
+  mimicServerCall()
+  .then(response => {
+    if (heart.innerText === EMPTY_HEART) {
+      heart.innerText = FULL_HEART
+      heart.classList.add('activated-heart')
+    } else {
+      heart.innerText = EMPTY_HEART
+      heart.classList.remove('activated-heart')
+    }
+  })
+  .catch(message => {
+    errorMessage.classList.remove('hidden')
+    errorMessage.innerText = message
+    setTimeout(() => { errorMessage.classList.add('hidden')}, 5000)
+  })
+}
 
 
 //------------------------------------------------------------------------------
